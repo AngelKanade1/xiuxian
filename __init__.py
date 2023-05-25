a6249d7d5a3fb871285a1e5f0f4812af5029e7b5
kanade <topjamesduan@gmail.com>
2023-05-25 10:26:23 +0800

    修改code格式，以后都按此格式加命令接口

--- __init__.py
+++ __init__.py
@@ -18,15 +18,8 @@ from main_func import *
 global_config = get_driver().config
 config = Config.parse_obj(global_config)
 
-# 指令触发
 gameStart = on_command("start xiuxian", permission=GROUP)
 @gameStart.handle()
-#GroupMessageEvent 代表只接受Group message。
-async def run_xiuxian_(bot: Bot, event: GroupMessageEvent):
-    back_msg = startGame()
-    
-    await bot.call_api('send_group_msg', **{
-    'group_id': event.group_id,
-    'message': back_msg
-    })
+async def game_start_(bot: Bot, event: GroupMessageEvent):
+    startGame(bot,event)    
     await gameStart.finish()
--- main_func.py
+++ main_func.py
@@ -1,3 +1,7 @@
-def startGame():
-    #todo
-    return "you have enter xiuxian world"
+async def startGame(bot,event):
+    back_msg = "you have enter xiuxian world"
+    
+    await bot.call_api('send_group_msg', **{
+        'group_id': event.group_id,
+        'message': back_msg
+    })
