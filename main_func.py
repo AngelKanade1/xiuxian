d4035b63ed6af30b752b355a5e1fc97c741d2ca3
kanade <topjamesduan@gmail.com>
2024-05-27 18:41:23 +0800

    New project

--- main_func.py
+++
@@ -1,7 +0,0 @@
-async def startGame(bot,event):
-    back_msg = "you have enter xiuxian world"
-    
-    await bot.call_api('send_group_msg', **{
-        'group_id': event.group_id,
-        'message': back_msg
-    })
